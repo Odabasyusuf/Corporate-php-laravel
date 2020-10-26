e99fe0a900c5831eacca937afef17c54df0648e0
Yusuf Odabas <yusuf.odabas.78@gmail.com>
2020-10-26 09:02:12 +0300

    temp

--- app/Helper/helpers.php
+++ app/Helper/helpers.php
@@ -1,21 +1,35 @@
 <?php
 
-    if(!function_exists('clearAllLogs')){
-        function clearAllLogs(){
-            Artisan::call("route:clear");
-            Artisan::call("config:clear");
-            Artisan::call("cache:clear");
-            Artisan::call("key:generate");
-        }
+if (!function_exists('clearAllLogs')) {
+    function clearAllLogs()
+    {
+        Artisan::call("route:clear");
+        Artisan::call("config:clear");
+        Artisan::call("cache:clear");
+        Artisan::call("key:generate");
     }
+}
 
-    if(!function_exists('checkDBConnection')){
-        function checkDBConnection(){
-            $link = @mysqli_connect(config('database.connections.' . env('DB_CONNECTION').'.host'),
-                    config('database.connections.' .env('DB_CONNECTION'). '.username'),
-                    config('database.connections' . env('DB_CONNECTION'). '.password'));
-            if ($link)
-                return mysqli_select_db($link, config('database.connections.'. env('DB_CONNECTION'). '.database'));
-            return false;
-        }
+if (!function_exists('checkDBConnection')) {
+    function checkDBConnection()
+    {
+        $link = @mysqli_connect(config('database.connections.' . env('DB_CONNECTION') . '.host'),
+            config('database.connections.' . env('DB_CONNECTION') . '.username'),
+            config('database.connections' . env('DB_CONNECTION') . '.password'));
+        if ($link)
+            return mysqli_select_db($link, config('database.connections.' . env('DB_CONNECTION') . '.database'));
+        return false;
     }
+}
+
+if (!function_exists('getVar')) {
+    function getVar($list)
+    {
+        $file = resource_path('vars/'.$list.'.json');
+        if(File::exists($file)){
+            //return file_get_contents($file);
+            return json_decode(file_get_contents($file),true);
+        }
+        return [];
+    }
+}
--- app/Http/Controllers/Admin/RouteController.php
+++ app/Http/Controllers/Admin/RouteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Spatie\Permission\Models\Role;
 
 class RouteController extends Controller
 {
@@ -15,6 +16,13 @@ class RouteController extends Controller
 
     public function index()
     {
+        $defaultSystemVars = getVar('system');
+        $roles = Role::all()->pluck("name")->all();
+        foreach ($defaultSystemVars['default_role'] as $value){
+           // return $value['name'];
+        }
+
+
         $routes = Route::getRoutes();
         return view('admin.routes.index', compact('routes'));
     }
